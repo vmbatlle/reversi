837a16c5cd47b2c749c8f81078982e18f592a854
Victor M. Batlle <736478@unizar.es>
2018-09-27 22:56:44 +0200

    Implementación original para timer0

---
+++ timer.h
@@ -0,0 +1,17 @@
+/*********************************************************************************************
+* Fichero:		timer.h
+* Autor:
+* Descrip:		funciones de control del timer0 del s3c44b0x
+* Version:
+*********************************************************************************************/
+
+#ifndef _TIMER_H_
+#define _TIMER_H_
+
+/*--- variables visibles del módulo timer.c/timer.h ---*/
+int switch_leds;
+
+/*--- declaracion de funciones visibles del módulo timer.c/timer.h ---*/
+void timer_init(void);
+
+#endif /* _TIMER_H_ */
