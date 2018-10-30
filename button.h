f745e6a0a516a9b95e7ffa7965b7ee6cdeb46125
unknown <diego.roymen@gmail.com>
2018-10-30 01:32:02 +0100

    Añadidas cabeceras a button.h

--- button.h
+++ button.h
@@ -1,16 +1,40 @@
 #ifndef _BUTTON_H_
 #define _BUTTON_H_
 
-/* TODO cabeceras */
+/*
+ * Estado de los botones de la placa:
+ * button_none: Ningún botón pulsado
+ * button_iz: Botón izquierdo pulsado
+ * button_dr: Botón derecho pulsado
+ */
 enum estado_button {button_none, button_iz, button_dr};
 
+/* TODO: comprobar que el derecho es EINT6 y el izquierdo EINT7 */
+
+/**
+ * @brief 	Configura los botones derecho e izquierdo
+ * 			(interrupciones externas EINT6/7, respectivamente)
+ * 			y activa su funcionamiento
+ */
 void button_iniciar(void);
 
+/**
+ * @brief 	Activa el funcionamiento de los botones y configura
+ * 			la función de callback.
+ * @param callback 	Función de callback a llamar cuando ocurra
+ * 					un evento de pulsación del botón.
+ * @warning	Una vez pulsado el botón no se registrarán más pulsaciones
+ * 			hasta que no se vuelva a llamar a @ref button_empezar
+ */
 void button_empezar(void (*callback)(enum estado_button));
 
+/**
+ * @brief 	Consulta el estado de los botones
+ * @return	button_none, button_iz o button_dr
+ */
 enum estado_button button_estado(void);
 
-/* TODO buscar un nombre mejor? y ponerlo en el .c */
-void button_tratar(enum estado_button button) __attribute__((interrupt("IRQ")));
+/* TODO: mover función de callback a otro fichero */
+void button_tratar(enum estado_button button);
 
 #endif /* _BUTTON_H_ */
