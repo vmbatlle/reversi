fa701c933066a79db0b5d9e3569476d629ac8614
unknown <diego.roymen@gmail.com>
2018-10-30 01:32:49 +0100

    Versión de pruebas de button.c + aclarados comentarios

--- button.c
+++ button.c
@@ -2,7 +2,7 @@
 #include "44blib.h"
 #include "44b.h"
 #include "button.h"
-//#include "8led.h"
+#include "8led.h"
 
 /* TODO quitar */
 /*--- variables globales del módulo ---*/
@@ -10,21 +10,20 @@
   Cuando se pulsa un botón sumamos y con el otro restamos. ¡A veces hay rebotes! */
 volatile static unsigned int int_count = 0;
 
-/* TODO volatile/static? */
-//enum estado_button button;
-void (*f_callback)(enum estado_button button);
+/* Estado de los botones */
+static enum estado_button estado_actual_button;
 
-/* TODO escribir 0xf en EXTINTPND en lugar de 0xC (las eint4/5/6/7 van juntas) */
+/* Función de callback */
+static void (*f_callback)(enum estado_button button);
 
 /* declaración de función que es rutina de servicio de interrupción
  * https://gcc.gnu.org/onlinedocs/gcc/ARM-Function-Attributes.html */
 void button_ISR(void) __attribute__((interrupt("IRQ")));
 
-/*--- codigo de funciones ---*/
 void button_ISR(void)
 {
 	/* Desactivar las interrupciones para la línea correspondiente a EINT4/5/6/7 */
-	//rINTMSK |= BIT_EINT4567; 		// Deshabilitar interrupciones línea EINT4/5/6/7
+	rINTMSK |= BIT_EINT4567; 		// Enmascarar interrupciones línea EINT4/5/6/7
 
 	int which_int = rEXTINTPND;
 	/* Identificar qué boton se ha pulsado */
@@ -38,8 +37,7 @@ void button_ISR(void)
 
 	/* La RSI debe poner a 0 el bit correspondiente de INTPND después de
 	 * limpiar los bits correspondientes de EXTINTPND */
-	rEXTINTPND |= 0xC;				// Pone a 0 los bits 2 y 3 (correspondientes a los botones
-									// derecho, EINT6 e izquierdo, EINT7 respectivamente)
+	rEXTINTPND |= 0xF;				// Pone a 0 los bits de EXTINTPND escribiendo 1s en el propio registro
 	rI_ISPC |= BIT_EINT4567;		// Poner a 0 el bit de INTPND
 									// correspondiente a la línea EINT4/5/6/7
 
@@ -49,9 +47,7 @@ void button_iniciar(void)
 {
 
 	/* Configuracion del controlador de interrupciones. Estos registros están definidos en 44b.h */
-	rEXTINTPND |= 0xC;				// Pone a 0 los bits 2 y 3 (correspondientes a los botones
-									// derecho, EINT6 e izquierdo, EINT7 respectivamente)
-									// de EXTINTPND escribiendo 1s en el propio registro
+	rEXTINTPND |= 0xF;				// Pone a 0 los bits de EXTINTPND escribiendo 1s en el propio registro
 	rI_ISPC |= BIT_EINT4567;		// Pone a 0 el bit correspondiente a las EINT4/5/6/7
 									// de INTPND escribiendo 1s en I_ISPC
 	rINTMOD &= ~(BIT_EINT4567);		// Configura la línea correspondiente a EINT4/5/6/7
@@ -66,10 +62,9 @@ void button_iniciar(void)
 
 	/* TODO: sería más correcto configurar primero el puerto y luego las interrupciones o da igual? */
 	/* Configuracion del puerto G */
-	rPCONG |= 0xF000;				// Establece la funcion de los pines 6 y 7 como EINT6-7
-	rPUPG &= ~(0xC0);				// Habilita el "pull up" de los puertos 6 y 7
-	/* TODO: mirar PDATG */
-	/* TODO: hay alguna forma mejor de hacer esto? */
+	rPCONG |= 0xF000;			// Establece la funcion de los pines 6 y 7 como EINT6-7
+	rPUPG &= ~(0xC0);			// Habilita el "pull up" de los puertos 6 y 7
+
 	rEXTINT &= 0x00FFFFFF;		// Configura las lineas 6 y 7 de int. como de flanco de bajada
 	rEXTINT |= 0x22000000;		// Los bits [30:28] corresponden a la línea 7 y
 								// los bits [26:24] a la línea 6. Ambos conjuntos de bits
@@ -77,7 +72,7 @@ void button_iniciar(void)
 								// el primer bit y se pone a 1 el segundo.
 
 	/* Por precaucion, se vuelven a borrar los bits de INTPND y EXTINTPND */
-	rEXTINTPND |= 0xC;
+	rEXTINTPND |= 0xF;
 	rI_ISPC |= BIT_EINT4567;
 
 }
@@ -88,27 +83,27 @@ void button_empezar(void (*callback)(enum estado_button))
 
 	/* Borrar los bits de INTPND y EXTINTPND por si había alguna petición
 	 * pendiente mientras estas estaban enmascaradas */
-	rEXTINTPND |= 0xC;				// Pone a 0 los bits 2 y 3 (correspondientes a los botones
-									// derecho, EINT6 e izquierdo, EINT7 respectivamente)
+	rEXTINTPND |= 0xF;				// Pone a 0 los bits de EXTINTPND escribiendo 1s en el propio registro
 	rI_ISPC |= BIT_EINT4567;		// Poner a 0 el bit de INTPND
 									// correspondiente a la línea EINT4/5/6/7
 	/* Activar las interrupciones para la línea correspondiente a EINT4/5/6/7 */
-	rINTMSK &= ~(BIT_EINT4567); 		// Habilitar interrupciones línea EINT4/5/6/7
+	rINTMSK &= ~(BIT_EINT4567); 	// Habilitar interrupciones línea EINT4/5/6/7
 }
 
 enum estado_button button_estado(void)
 {
-	// TODO: completar
-	return button_none;
+	return estado_actual_button;
 }
 
 void button_tratar(enum estado_button button)
 {
+	estado_actual_button = button;
+
 	// TODO: quitar int_count y el 8led, estan solo por tests
 	if (button == button_iz) {
 		int_count--;
 	} else if (button == button_dr) {
 		int_count++;
 	}
-	//D8Led_symbol(int_count & 0x000f); // sacamos el valor por pantalla (módulo 16)
+	D8Led_symbol(int_count & 0x000f); // sacamos el valor por pantalla (módulo 16)
 }
