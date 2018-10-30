86c9bf89dfaf04eca90e6b6228f1479ba84cbf28
Victor M. Batlle <736478@celes.unizar.es>
2018-10-30 13:16:53 +0100

    Completada la maquina de estados de botones_antirebotes

--- botones_antirebotes.c
+++ botones_antirebotes.c
@@ -9,14 +9,33 @@ static enum estado_rebotes estado;
 static const unsigned int trp = 20;
 static const unsigned int trd = 20;
 
-static unsigned int nextTime = 0;
+static unsigned int timeAntes = 0;
+static enum pulsacion pulsado_return;
 
-void (*const tabla_estados [MAX_STATES]) (void) = {
-	action_unpressed, action_wait_trp, action_pressed, action_wait_trd
+enum pulsacion action_unpressed (void);
+enum pulsacion action_wait_trp (void);
+enum pulsacion action_pressed (void);
+enum pulsacion action_wait_release (void);
+enum pulsacion action_wait_trd (void);
+enum pulsacion (*const tabla_estados [MAX_STATES]) (void) = {
+	action_unpressed, action_wait_trp, action_pressed, action_wait_release, action_wait_trd
 };
 
 
-enum pulsacion check_pulsacion(void) {
+void antirebotes_callback(enum estado_button button) {
+	if (estado == unpressed) {
+		estado = wait_trp;
+		pulsado_return = button == button_iz ? pulsacion_iz : pulsacion_dr;
+		timeAntes = timer0_leer();
+	}
+}
+
+void antirebotes_iniciar(void) {
+	estado = unpressed;
+	button_empezar(antirebotes_callback);
+}
+
+enum pulsacion antirebotes_gestionar(void) {
 	if (estado >= 0 && estado < MAX_STATES) {
 		return tabla_estados[estado]();
 	} else {
@@ -28,41 +47,39 @@ enum pulsacion check_pulsacion(void) {
 /* TODO: cabeceras */
 
 enum pulsacion action_unpressed (void) {
-	enum estado_button button = button_estado();
-	if (button != button_none) {
-		timer0_empezar();
-		estado = wait_trp;
-		return button == button_iz ? pulsacion_iz : pulsacion_dr;
-	} else {
-		return pulsacion_none;
-	}
+	return pulsacion_none;
 }
 
 enum pulsacion action_wait_trp (void) {
-	unsigned int time = timer0_leer();
-	if (time >= trp) {
-		nextTime = time + 20; /* TODO: Ahora son 20 ticks, deberian ser 20 ms */
+	unsigned int timeAhora = timer0_leer();
+	if (timeAhora - timeAntes >= trp) {
+		timeAntes = timeAhora; /* TODO: Ahora son 20 ticks, deberian ser 20 ms */
 		estado = pressed;
 	}
 	return pulsacion_none;
 }
 
 enum pulsacion action_pressed (void) {
-	unsigned int time = timer0_leer();
-	if (time >= nextTime) { /* TODO: Comprobar overflow */
+	estado = wait_release;
+	return pulsado_return;
+}
+
+enum pulsacion action_wait_release (void) {
+	unsigned int timeAhora = timer0_leer();
+	if (timeAhora - timeAntes >= 20) { /* TODO ticks */
 		if (button_estado() == button_none) {
-			nextTime = time + trd;
-		} else {
-			nextTime = time + 20; /* TODO: Ahora son 20 ticks, deberian ser 20 ms */
+			estado = wait_trd;
 		}
+		timeAntes = timeAhora;
 	}
 	return pulsacion_none;
 }
 
 enum pulsacion action_wait_trd (void) {
-	unsigned int time = timer0_leer();
-	if (time >= nextTime) {
-		button_empezar();
+	unsigned int timeAhora = timer0_leer();
+	if (timeAhora - timeAntes >= trd) {
+		button_empezar(antirebotes_callback);
+		estado = unpressed;
 	}
 	return pulsacion_none;
 }
--- botones_antirebotes.h
+++ botones_antirebotes.h
@@ -5,13 +5,14 @@
  * TODO
  * Estado de la máquina de estados:
  */
-enum estado_rebotes { unpressed, wait_trp, pressed, wait_trd, MAX_STATES };
+enum estado_rebotes { unpressed, wait_trp, pressed, wait_release, wait_trd, MAX_STATES };
 
 /* TODO cambiar los nombres de los enum */
 
 enum pulsacion {pulsacion_none, pulsacion_iz, pulsacion_dr};
 
 /* TODO cabecera */
-enum pulsacion check_pulsacion(void);
+void antirebotes_iniciar(void);
+enum pulsacion antirebotes_gestionar(void);
 
 #endif /* __BOTONES_ANTIREBOTES_H__ */
