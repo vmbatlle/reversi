7bec89291675f04c98539fc0d5fa74d7fb84d797
unknown <diego.roymen@gmail.com>
2018-12-05 01:43:12 +0100

    Mejora de las funciones de dibujo (tablero, fichas)

--- reversi_gui.h
+++ reversi_gui.h
@@ -6,8 +6,19 @@
 #ifndef __REVERSI_GUI__
 #define __REVERSI_GUI__
 
+/* TODO merge de este enum con el de reversi_main, decidir como hacerlo */
+enum contenido_casilla {
+	VACIA = 0,
+	BLANCA = 1,
+	NEGRA = 2,
+	GRIS = 3
+};
+
 void gui_inicializar();
 void gui_limpiar_pantalla();
 void gui_dibujar_tablero_vacio();
+/* TODO cambiar el 8 por un enum */
+void gui_dibujar_tablero_completo(char* tablero[][8]);
+void gui_dibujar_ficha(int fila, int columna, enum contenido_casilla casilla);
 
 #endif /* __REVERSI_GUI__ */
